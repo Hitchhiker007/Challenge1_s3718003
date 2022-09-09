2a4726f33c788f2a305dba270ddef1775a0430d7
Will <80670903+Hitchhiker007@users.noreply.github.com>
2022-09-09 16:08:59 +1000

    Add files via upload
    item class

---
+++ src/main/java/com/example/engineering_process_tools/model/item.java
@@ -0,0 +1,54 @@
+package com.example.engineering_process_tools.model;
+
+public class item
+{
+    int id;
+    String name;
+    String desc;
+    double price;
+
+    public item(int id, String name, String desc, double price) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+}
