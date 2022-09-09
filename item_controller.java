66b6e3c92860daad75d97576902cb4bb051cff9b
Will <80670903+Hitchhiker007@users.noreply.github.com>
2022-09-09 13:09:22 +1000

    Add files via upload

---
+++ item_controller.java
@@ -0,0 +1,37 @@
+package com.example.s3718003_challenge1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/item")
+public class item_controller {
+
+    @GetMapping
+    public String getItem()
+    {
+        return "http GET reqeust was sent";
+    }
+
+    @PostMapping
+    public String createItem()
+    {
+        return "http POST reqeust was sent";
+    }
+
+    @PutMapping
+    public String updateItem()
+    {
+        return "http PUT reqeust was sent";
+    }
+
+    @DeleteMapping
+    public String deleteItem()
+    {
+        return "http DELETE reqeust was sent";
+    }
+}
