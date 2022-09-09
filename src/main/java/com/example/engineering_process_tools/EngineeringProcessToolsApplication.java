21551ad52777b6c1ad88603498453009b1e7b5a6
Will <billywells99@hotmail.com>
2022-09-09 12:22:44 +1000

    Second commit

---
+++ src/main/java/com/example/engineering_process_tools/EngineeringProcessToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.engineering_process_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineeringProcessToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngineeringProcessToolsApplication.class, args);
+    }
+
+}
